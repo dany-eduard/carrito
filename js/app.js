938f3b7ff0309802b5e4ae82d1db182dddb3760f
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-06-15 17:42:20 -0500

    Se añadió la función insertarCarrito()

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const carrito = document.getElementById("carrito");
 const cursos = document.getElementById("lista-cursos");
+const listaCursos = document.querySelector("#lista-carrito tbody");
 
 eventListeners();
 function eventListeners() {
@@ -11,8 +12,32 @@ function comprarCurso(e) {
   if (e.target.classList.contains("agregar-carrito")) {
     //Si elemento en el que se hace click tiene la clase agregar-carrito ...
     const curso = e.target.parentElement.parentElement; //Se usa delegation y se almacena el card del curso seleccionado en la variable
-    leerDatosCurso();
+    leerDatosCurso(curso);
   }
 }
 
-function leerDatosCurso() {}
+function leerDatosCurso(curso) {
+  //Esta función se encarga de tomar datos del curso seleccionado y almacenarlos como vector en la variable curso
+  const infoCurso = {
+    imagen: curso.querySelector("img").src,
+    titulo: curso.querySelector("h4").textContent,
+    precio: curso.querySelector(".precio span").textContent,
+    id: curso.querySelector("a").getAttribute("data-id"),
+  };
+  insertarCarrito(infoCurso);
+}
+
+function insertarCarrito(curso) {
+  const row = document.createElement("tr");
+  row.innerHTML = `
+    <td>
+      <img src="${curso.imagen}" width=100> 
+    </td>
+    <td>${curso.titulo}</td>
+    <td>${curso.precio}</td>
+    <td>
+      <a href="#" class="borrar-curso" data-id="${curso.id}"> x </a>
+    </td>
+  `;
+  listaCursos.appendChild(row);
+}
